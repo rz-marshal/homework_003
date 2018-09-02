03f1e58c60d1227c57ae483e5a304d20d83a6c7d
V. Marshal <41177672+rz-marshal@users.noreply.github.com>
2018-09-03 03:35:37 +1100

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,3 @@
 <?php
 
 require_once "src/functions.php";
-
-//запуск буферизации вывода
-ob_start();
-
-
-
-?>
-
-<!doctype html>
-<html lang="ru">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>ДЗ №3, Владислав Маршал</title>
-</head>
-<body>
-<div class="container">
-    <div class="row">
-            <h1>Вывод XML</h1>
-            <?= $content ?>
-    </div>
-</div>
-</body>
-</html>
